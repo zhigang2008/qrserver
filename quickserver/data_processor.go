6bfac600db67284410014f2ec26bdd8dc3d82a3b
Steven <zhigang2008@gmail.com>
2013-08-26 10:09:01 +0800

    rename filename

--- quickserver/data_processor.go
+++ quickserver/data_processor.go
@@ -7,8 +7,8 @@ import (
 
 //解析数据
 func DataProcess(content []byte, datamgr *DataManager) (err error) {
+	log.Info("Begin process data")
 	data := new(DataAlert)
-	log.Infof("cont:%s", string(content[0:]))
 	data.SeqNo = string(content[0:1])
 	datamgr.DataSave(data)
 	return
