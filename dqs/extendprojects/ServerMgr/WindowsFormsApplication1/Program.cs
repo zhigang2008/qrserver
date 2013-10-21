8542466775d2e85d92ae1d2c20590c3b75cdd811
zhigang <zhigang2008@gmail.com>
2013-10-21 11:15:30 +0800

    servermgr solution modify

--- dqs/extendprojects/ServerMgr/WindowsFormsApplication1/Program.cs
+++ dqs/extendprojects/ServerMgr/WindowsFormsApplication1/Program.cs
@@ -1,80 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Diagnostics;
+using System.Threading; 
 
 
 namespace WindowsFormsApplication1
 {
     static class Program
     {
+
+        public static EventWaitHandle ProgramStarted;
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Process instance = RunningInstance();
-            if (instance == null)
+            // 尝试创建一个命名事件  
+            bool createNew;
+            ProgramStarted = new EventWaitHandle(false, EventResetMode.AutoReset, "DQS_Server_Instance", out createNew);
+
+            // 如果该命名事件已经存在(存在有前一个运行实例)，则发事件通知并退出  
+            if (!createNew)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new frameMain());
+                ProgramStarted.Set();
+                return;
             }
-            else
-            {
-                HandleRunningInstance(instance);
-            }
-            
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frameMain());
         }
 
-         #region  确保程序只运行一个实例
-         private static Process RunningInstance()
-         {
-             Process current = Process.GetCurrentProcess();
-             Process[] processes = Process.GetProcessesByName(current.ProcessName);
-             //遍历与当前进程名称相同的进程列表  
-             foreach (Process process in processes)
-             {
-                 //如果实例已经存在则忽略当前进程  
-                 if (process.Id != current.Id)
-                 {
-                     //保证要打开的进程同已经存在的进程来自同一文件路径
-                     if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == current.MainModule.FileName)
-                     {
-                         //返回已经存在的进程
-                         return process;
-                         
-                     }
-                 }
-             }
-             return null;
-         }
- 
-         private static void HandleRunningInstance(Process instance)
-         {
-             MessageBox.Show("已经在运行！", instance.MainWindowHandle.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Information);
-             IntPtr hWnd = instance.MainWindowHandle;
-             SendMessage(instance.MainWindowHandle, WM_SHOWWINDOW, 1, 0);
-             ShowWindowAsync(hWnd, 1);  //调用api函数，正常显示窗口
-             SetForegroundWindow(hWnd); //将窗口放置最前端
-         }
-
-         [DllImport("User32.dll")]
-         private static extern bool ShowWindowAsync(System.IntPtr hWnd, int cmdShow);
-         [DllImport("User32.dll")]
-         private static extern bool SetForegroundWindow(System.IntPtr hWnd);
-         [DllImport("user32.dll", EntryPoint = "SendMessage")]
-         private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
-         [DllImport("user32.dll")]
-         private static extern bool IsIconic(IntPtr hWnd);
-
-         private const int WM_SHOWWINDOW = 24;
-         private const int SW_SHOWNORMAL = 10;
-
-         #endregion
     }
 }
