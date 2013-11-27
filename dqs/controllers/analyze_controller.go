b49a3a3d8ed10d1781ee3baa5cf6e21e9f7489c9
Steven <zhigang2008@gmail.com>
2013-11-27 18:45:48 +0800

    add 分析页面

--- dqs/controllers/analyze_controller.go
+++ dqs/controllers/analyze_controller.go
@@ -1,11 +1,10 @@
 package controllers
 
 import (
-//"dqs/dao"
-//"dqs/util"
-//"github.com/astaxie/beego"
-
-//log "github.com/cihub/seelog"
+	//"dqs/dao"
+	//"dqs/util"
+	"github.com/astaxie/beego"
+	log "github.com/cihub/seelog"
 )
 
 type AnalyzeController struct {
@@ -17,6 +16,19 @@ func (this *AnalyzeController) Get() {
 	this.Data["title"] = "数据分析"
 	this.Data["author"] = "wangzhigang"
 	this.CheckUser()
-	this.TplNames = "analyze.html"
+
+	usegis, err := beego.AppConfig.Bool("map.gis")
+	if err != nil {
+		usegis = false
+		log.Warnf("无法从配置文件中获取gis启用信息.将使用地图模式.")
+	}
+	if usegis {
+		this.Data["gisServiceUrl"] = beego.AppConfig.String("gis.service.url")
+		this.Data["gisBasicLayer"] = beego.AppConfig.String("gis.layer.basic")
+		this.TplNames = "analyze-gis.html"
+	} else {
+
+		this.TplNames = "analyze.html"
+	}
 	this.Render()
 }
